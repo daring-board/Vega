afaa2f2236fcbd27a871725f396ac97c82dec7d3
daring-board <mahotox101500@gmail.com>
2019-06-29 20:39:47 +0900

    プロジェクトを修正

--- public/registevents.js
+++ public/registevents.js
@@ -1,12 +1,15 @@
 AFRAME.registerComponent('registerevents', {
 	init: function () {
 		var marker = this.el;
+		var name = 'flower';
+		localStorage.setItem(name, 0);
 		marker.addEventListener('markerFound', function() {
 			var markerId = marker.id;
 			console.log('markerFound', markerId);
 			// TODO: Add your own code here to react to the marker being found.
 			this.markerVisible = true;
-			v = document.querySelector('#flower');
+			v = document.querySelector('#'+name);
+			localStorage.setItem(name, localStorage.getItem(name)++);
 		});
 		marker.addEventListener('markerLost', function() {
 			var markerId = marker.id;
@@ -23,12 +26,15 @@ AFRAME.registerComponent('hiroevents', {
 	init: function () {
 		var marker = this.el;
 		console.log(marker.id);
+		var name = 'dish';
+		localStorage.setItem(name, 0);
 		marker.addEventListener('markerFound', function() {
 			var markerId = marker.id;
 			console.log('markerFound', markerId);
 			// TODO: Add your own code here to react to the marker being found.
 			this.markerVisible = true;
-			v = document.querySelector('#dish');
+			v = document.querySelector('#'+name);
+			localStorage.setItem(name, localStorage.getItem(name)++);
 		});
 		marker.addEventListener('markerLost', function() {
 			var markerId = marker.id;
