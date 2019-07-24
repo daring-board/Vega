701939515c2e2a95a91f80e6bbe1637931080c4a
iwasa_ma <iwasa@tasdg.co.jp>
2019-07-24 10:55:42 +0900

    修正

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Our Party"/>
+    <b-container>
+      <b-row>
+        <img class="top-img" src="./assets/toppage_3.png"/>
+      </b-row>
+        <HelloWorld
+          msg="Welcome to Our Party"
+        />
+    </b-container>
   </div>
 </template>
 
@@ -24,7 +31,11 @@ export default {
   color: #2c3e50;
   width: 100vw;
   height: 100vh;
-  background-image: url("./assets/toppage_3.png");
-  background-size: cover;
+  /* background-image: url("./assets/toppage_3.png");
+  background-size:80% auto; */
+}
+.top-img{
+  width: auto;
+  height: 90vh;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <b-container>
-      <h1>{{ msg }}</h1>
       <p>
         <b-button @click="clickARCamera">ARカメラ</b-button>
       </p>
@@ -52,17 +51,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
 }
